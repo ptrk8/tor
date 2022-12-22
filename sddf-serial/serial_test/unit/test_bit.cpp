741d3e24942c5cc0b89a79bb5d11a67acdbc894e
Patrick Hao <patrick.hao95@gmail.com>
2022-12-22 15:50:32 +1100

    Wrote some basic unit tests for bit.c

--- sddf-serial/serial_test/unit/test_bit.cpp
+++ sddf-serial/serial_test/unit/test_bit.cpp
@@ -4,12 +4,37 @@ extern "C" {
 #include <bit.h>
 }
 
-
-TEST(test_bit, should_set_bit) {
-    int val = BIT(0);
-    printf("val: 0x%x", val);
-    val = BIT(3);
-    printf("val: 0x%x", val);
+TEST(test_bit, should_set_first_bit) {
+    ASSERT_EQ(
+            0b00000000000000000000000000000001,
+            BIT(0)
+    );
 }
 
+TEST(test_bit, should_set_second_bit) {
+    ASSERT_EQ(
+            0b00000000000000000000000000000010,
+            BIT(1)
+    );
+}
 
+TEST(test_bit, should_set_third_bit) {
+    ASSERT_EQ(
+            0b00000000000000000000000000000100,
+            BIT(2)
+    );
+}
+
+TEST(test_bit, should_set_32nd_bit) {
+    ASSERT_EQ(
+            0b10000000000000000000000000000000,
+            BIT(31)
+    );
+}
+
+TEST(test_bit, should_set_64th_bit) {
+    ASSERT_EQ(
+            0b1000000000000000000000000000000000000000000000000000000000000000,
+            BIT(63)
+    );
+}
